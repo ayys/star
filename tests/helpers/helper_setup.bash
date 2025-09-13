33f0244030b42888155aec577dc446d0f6325c8f
Fruchix <fruchix@gmail.com>
2025-09-13 16:47:36 +0200

    tests: improve and add helpers

--- tests/helpers/helper_setup.bash
+++ tests/helpers/helper_setup.bash
@@ -6,6 +6,10 @@ setup_common() {
 setup_tmpdir() {
   export TEST_ROOT="$(mktemp -d)"
   export _STAR_HOME="$TEST_ROOT/.star"
+  export _STAR_DISPLAY_FORMAT="<INDEX>: ${_STAR_COLOR_STAR}%f${_STAR_COLOR_RESET} -> ${_STAR_COLOR_PATH}%l${_STAR_COLOR_RESET}"
+  export _STAR_EXPORT_ENV_VARIABLES=yes
+  # export _STAR_DISPLAY_FORMAT="${_STAR_COLOR_STAR}%f${_STAR_COLOR_RESET} -> ${_STAR_COLOR_PATH}%l${_STAR_COLOR_RESET}"
+  # export _STAR_EXPORT_ENV_VARIABLES=no
 
   setup_common
 }
