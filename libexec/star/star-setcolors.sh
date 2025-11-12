74a9185d6eabc77495cc9bbfc0d748e75bc2543d
Fruchix <fruchix@gmail.com>
2025-11-12 17:38:09 +0100

    fix: properly declare default colors

--- libexec/star/star-setcolors.sh
+++ libexec/star/star-setcolors.sh
@@ -6,14 +6,14 @@
 if [[ -t 1 ]]; then
     if [[ "$COLORTERM" = "truecolor" || "$COLORTERM" = "24bit" ]]; then
         # Use true color (=24 bits colors)
-        export __STAR_COLOR_NAME="${__STAR_COLOR_NAME-$'\033[38;2;255;131;0m'}"
-        export __STAR_COLOR_PATH="${__STAR_COLOR_PATH-$'\033[38;2;1;169;130m'}"
-        export __STAR_COLOR_RESET="${__STAR_COLOR_RESET-$'\033[0m'}"
+        export __STAR_COLOR_NAME=${__STAR_COLOR_NAME-$'\033[38;2;255;131;0m'}
+        export __STAR_COLOR_PATH=${__STAR_COLOR_PATH-$'\033[38;2;1;169;130m'}
+        export __STAR_COLOR_RESET=${__STAR_COLOR_RESET-$'\033[0m'}
     else
         # Fallback to 256-color codes
-        export __STAR_COLOR_NAME="${__STAR_COLOR256_NAME-$'\033[38;5;214m'}"
-        export __STAR_COLOR_PATH="${__STAR_COLOR256_PATH-$'\033[38;5;36m'}"
-        export __STAR_COLOR_RESET="${__STAR_COLOR256_RESET-$'\033[0m'}"
+        export __STAR_COLOR_NAME=${__STAR_COLOR256_NAME-$'\033[38;5;214m'}
+        export __STAR_COLOR_PATH=${__STAR_COLOR256_PATH-$'\033[38;5;36m'}
+        export __STAR_COLOR_RESET=${__STAR_COLOR256_RESET-$'\033[0m'}
     fi
 else
     # No terminal detected, disable colors
