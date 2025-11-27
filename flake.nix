11c8c14b7b3c0c341b06ca151000b852db44f30e
Ayush Jha <ayys@duck.com>
2025-11-27 22:33:58 +0545

    feat: peg to nix 25.11
    Actually, it makes sense to peg it to nix 25.11 instead of unstable
    because unstable tends to change quite a bit.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Unix command line bookmark manager.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     flakeutils.url = "github:numtide/flake-utils";
   };
 
